059f558dae58d7b60cd7a29ef065b57f2305fd73
Amimul Basher <amimul.basher@bjitgroup.com>
2023-06-09 16:56:45 +0600

    Jekins demo application created

---
+++ src/test/java/com/amimul/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,19 @@
+package com.amimul.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+	public static final Logger logger = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
+	@Test
+	void contextLoads() {
+		logger.info("The test case is executing");
+		assertEquals(true, true);
+	}
+}
