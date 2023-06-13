19ea447d86f12535efa0863412de16a4b16b024c
Amimul Basher <amimul.basher@bjitgroup.com>
2023-06-13 17:46:00 +0600

    Compilation error resolved

--- src/main/java/com/amimul/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/amimul/jenkinsdemo/JenkinsDemoApplication.java
@@ -9,10 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsDemoApplication {
 	public static final Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 7dd69e2ea754307a40aa10e1275ce274113117c5
 	public void init(){
 		logger.info("The application is getting started");
 	}
@@ -20,9 +16,4 @@ public class JenkinsDemoApplication {
 		logger.info("The application is running");
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 7dd69e2ea754307a40aa10e1275ce274113117c5
 }
